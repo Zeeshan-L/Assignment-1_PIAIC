46d6e7458a674776efe6849fdb2817bf6754647d
Zeeshan Saeed <84595628+Zeeshan-L@users.noreply.github.com>
2022-03-05 20:14:28 +0500

    another

---
+++ index7.js
@@ -0,0 +1,8 @@
+//question 7
+const heading = document.getElementById("heading");
+document.write(heading);
+// document.write("<br> Variable names can only contain,numbers,$ and _.");
+// document.write("<br> For example $my_1stVariable.");
+// document.write("<br> Variable must begin with a letter,$ or _.");
+// document.write("<br> For exapmle: $name,_name or name");
+// document.write("<br> Variable names should not be JS keywords");
