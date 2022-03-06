3b0843fafe3297186df4d7692de9179bdf211293
Zeeshan Saeed <84595628+Zeeshan-L@users.noreply.github.com>
2022-03-07 00:39:01 +0500

    assignment 1

---
+++ index8.js
@@ -0,0 +1,5 @@
+// Question No:8
+let a = 3;
+let b = 5;
+let sum = a + b;
+document.write(` sum of ${a} and ${b} is ${sum}`);
---
+++ index9.js
@@ -0,0 +1,17 @@
+// question no:9
+let a = 3;
+let b = 5;
+let sum = a - b;
+document.write(` sub of ${a} and ${b} is ${sum}`);
+let c = 3;
+let d = 5;
+let sum1 = a * b;
+document.write(` <br> multi of ${c} and ${d} is ${sum1}`);
+let e = 3;
+let f = 5;
+let sum2 = a % b;
+document.write(` <br> mod of ${e} and ${f} is ${sum2}`);
+let g = 3;
+let h = 5;
+let sum3 = a / b;
+document.write(` <br> division of ${g} and ${h} is ${sum3}`);
