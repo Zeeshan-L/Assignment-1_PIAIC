efd42faca90bd4eb38fb86cda60500fa392b0090
Zeeshan Saeed <84595628+Zeeshan-L@users.noreply.github.com>
2022-03-09 23:03:30 +0500

    Assignment-1

---
+++ index11.js
@@ -0,0 +1,9 @@
+// Question no:11
+let c = 25;
+let f = 70;
+let temp;
+temp = (c * 9) / 5 + 32;
+document.write(`${c} Celsius is ${temp} Fahrenhelt`);
+let temp2;
+temp2 = ((f - 32) * 5) / 9;
+document.write(`<br>${f} Fahrenheit is ${temp2} Celsius`);
